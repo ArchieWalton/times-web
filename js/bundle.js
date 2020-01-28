0630836036ef376c21373ee451d748db53aeedb0
ParadoxEpcot <59997116+ParadoxEpcot@users.noreply.github.com>
2020-01-28 10:42:20 +0000

    test4

--- js/bundle.js
+++ js/bundle.js
@@ -124158,35 +124158,5 @@ function defaultCallback(err) {
 var net = require('net');
 const Themeparks = require("themeparks");
 document.getElementById("20").innerHTML = "Test";
-const code = 212;
-const park = Math.floor(code/100);
-const ride = code % 100;
-
-if (park == 1){
-  const myPark = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
-}else if (park == 2){
-  var myPark = new Themeparks.Parks.WaltDisneyWorldEpcot();
-}else if (park == 3){
-  const myPark = new Themeparks.Parks.WaltDisneyWorldHollywoodStudios();
-}else if (park == 4){
-  var myPark = new Themeparks.Parks.WaltDisneyWorldAnimalKingdom();
-};
-var addID = 0;
-var totalID = 0;
-// Access wait times by Promise
-const CheckWaitTimes = () => {
-    myPark.GetWaitTimes().then((rideTimes) => {
-        rideTimes.forEach((ride) => {
-          document.write("Test1 achieved")
-          var currentID = park.toString();
-          totalID = currentID + addID.toString();
-          document.getElementById(totalID).innerHTML = ride.waitTime;
-          addID++;
-        });
-    }).catch((error) => {
-        console.error(error);
-    })
-};
-CheckWaitTimes();
 
 },{"net":1,"themeparks":664}]},{},[721]);
--- js/update.js
+++ js/update.js
@@ -1,1 +1,3 @@
+var net = require('net');
+const Themeparks = require("themeparks");
 document.getElementById("20").innerHTML = "Test";
